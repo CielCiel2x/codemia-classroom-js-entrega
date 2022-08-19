2bf51b490de4974578c6d4c53c3fd0dfa0b6a61d
Cristopher Yang <dg.cris.yang@gmail.com>
2022-08-19 00:00:06 -0300

    eliminar nesting de condiciones en ejercicio 05 de clase 03

--- class-3/ejercicio-5.js
+++ class-3/ejercicio-5.js
@@ -1,12 +1,9 @@
 const auth = (age, acceptedTOS) => {
-    if (age > 13) {
-      if (acceptedTOS) {
+    if (age >= 13 && acceptedTOS) {
         return true;
-      }
+    } else if (age > 13 && !acceptedTOS) {
       return "tenés que aceptar los términos y condiciones para ingresar";
-    }
-  
-    if (acceptedTOS) {
+    } else if (age < 13 && acceptedTOS) {
       return "tenes que ser mayor de 13 para ingresar";
     }
     return "tenés que ser mayor de 13 y aceptar los terminos y condiciones para ingresar";
@@ -16,4 +13,3 @@ const auth = (age, acceptedTOS) => {
   console.log(auth(25, false));
   console.log(auth(2, true));
   console.log(auth(2, false));
-  
