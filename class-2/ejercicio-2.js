c06212d4432525e76c19211b7f6c15887f8b0307
Cristopher Yang <dg.cris.yang@gmail.com>
2022-08-18 23:43:57 -0300

    eliminar anidaciones innecesarias en ejercicio 02 de la clase 2

--- class-2/ejercicio-2.js
+++ class-2/ejercicio-2.js
@@ -1,27 +1,22 @@
 const mostrarMensajeSegunEdadIf = (edad) => {
-    if (edad >= 18) {
-      // Anidé el if de 90 años para que no tenga que verificarlo si no es mayor de edad, no se si es mas optimo o no pero tengo entendido que así se evita hacer una verificacion extra cuando no es necesario
-      if (edad > 90) {
-        return "Que edad tan inusual, puede pasar!";
-      }
-  
-      return "Bienvenido, puede pasar";
-    } else {
-      return "Lo siento, eres menor de edad";
-    }
+  if (edad > 90) {
+    return "Que edad tan inusual, puede pasar!";
+  } else if (edad >= 18) {
+    return "Bienvenido, puede pasar";
+  } else {
+    return "Lo siento, eres menor de edad";
+  }
   };
   
   console.log(mostrarMensajeSegunEdadIf(90));
   
   const mostrarMensajeSegunEdadTernario = (edad) => {
     let mensajePermiso =
-      edad >= 18
-        ? edad >= 90
-          ? "Que edad tan inusual, puede pasar!"
-          : "Bienvenido, puede pasar"
-        : "Lo siento, eres menor de edad";
-  
+      edad > 90 ? "Que edad tan inusual, puede pasar!" :
+      edad >= 18 ? "Bienvenido, puede pasar" :
+      "Lo siento, eres menor de edad";
+
     return mensajePermiso;
-  };
+  }
   
   console.log(mostrarMensajeSegunEdadTernario(90));
